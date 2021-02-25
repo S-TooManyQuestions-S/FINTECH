fa41dfbf050f7c7c030d3ca272547e89c02a9a2f
toomanyquestions <avsamarenko_1@edu.hse.ru>
2021-02-25 22:58:58 +0300

    HomeWork 2 is done

--- TinkoffChat/TinkoffChat/ViewController.swift
+++ TinkoffChat/TinkoffChat/ViewController.swift
@@ -2,60 +2,45 @@
 //  ViewController.swift
 //  TinkoffChat
 //
-//  Created by Андрей Самаренко on 16.02.2021.
+//  Created by Андрей Самаренко on 25.02.2021.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController : UIViewController{
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        if(log){
-            print("UIViewController moved from <Disappeared> to <Appearing>: \(#function)")
-        }
+            Logger.logProcess(fullDescription: "UIViewController moved from <Disappeared> to <Appearing>: \(#function)")
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        if(log){
-            print("UIViewController moved from <Appearing> to <Appeared>: \(#function)")
-        }
+            Logger.logProcess(fullDescription: "UIViewController moved from <Appearing> to <Appeared>: \(#function)")
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        if(log){
-            print("UIViewController: \(#function)")
-        }
+            Logger.logProcess(fullDescription: "UIViewController: \(#function)")
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        if(log){
-            print("UIViewController: \(#function)")
-        }
+        Logger.logProcess(fullDescription: "UIViewController: \(#function)")
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        if(log){
-            print("UIViewController moved from <Appeared> to <Disappearing>: \(#function)")
-        }
+        Logger.logProcess(fullDescription: "UIViewController moved from <Appeared> to <Disappearing>: \(#function)")
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        if(log){
-            print("UIViewController moved from <Disappearing> to <Disappeared>: \(#function)")
-        }
+        Logger.logProcess(fullDescription: "UIViewController moved from <Disappearing> to <Disappeared>: \(#function)")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if(log){
-            print("UIViewController moved from <Loading View> to <Loaded View>: \(#function)")
-        }
+        Logger.logProcess(fullDescription: "UIViewController moved from <Loading View> to <Loaded View>: \(#function)")
     }
 }
-
